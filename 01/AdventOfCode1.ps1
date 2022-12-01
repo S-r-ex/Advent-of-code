d7ba8726bf38b8e5c2b67c388c6f37197b4ee9f5
S-r-ex <82445044+S-r-ex@users.noreply.github.com>
2022-12-01 21:40:16 +0100

    Add files via upload

---
+++ 01/AdventOfCode1.ps1
@@ -0,0 +1,17 @@
+﻿$file_input = Get-Content "$PSScriptRoot\caliorielves.txt"
+
+$elfarray = @()
+
+foreach ($item in $file_input) {
+    $carriedcalories += $item
+    if ($item -eq "") {
+        $elfarray += $carriedcalories
+        $carriedcalories = 0
+    }
+}
+
+$sortedelves = $elfarray | sort -Descending
+
+"The elf with the most calories has " + $sortedelves[0] + " calories"
+
+"The top three has a total of " + ($sortedelves[0..2] | Measure-Object -sum).sum
